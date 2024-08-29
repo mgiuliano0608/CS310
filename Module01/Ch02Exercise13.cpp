b8d9bee6f380ca5f1f14119936eb4714f97b97ea
Michael Giuliano <michael.giuliano.0608@gmail.com>
2024-08-28 21:30:21 -0400

    Created assignment 1.2

---
+++ Module01/Ch02Exercise13.cpp
@@ -0,0 +1,40 @@
+//*********************************************************
+// Michael Giuliano
+// CS310-T301
+// M1: Assignment 1.2
+//*********************************************************
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float originalPrice;
+    float markupPercentage;
+    float salesTaxPercentage;
+
+    cout << "Enter the original price (Ex. 200.00): ";
+    cin >> originalPrice;
+    cout << "Enter the markup percentage (Ex. 20.0): ";
+    cin >> markupPercentage;
+    cout << "Enter the sales tax percentage (Ex. 6.0): ";
+    cin >> salesTaxPercentage;
+
+    float markupRate = markupPercentage / 100;
+    float salePrice = originalPrice * (1 + markupRate);
+    float salesTaxRate = salesTaxPercentage / 100;
+    float salesTax = salePrice * salesTaxRate;
+    float totalPrice = salePrice + salesTax;
+
+    cout << endl << fixed << setprecision(2);
+    cout << "Original price: $" << originalPrice << endl;
+    cout << "Markup percentage: " << markupPercentage << "%" << endl;
+    cout << "Sale price: $" << salePrice << endl;
+    cout << "Sales tax rate: " << salesTaxPercentage << "%" << endl;
+    cout << "Sales tax: $" << salesTax << endl;
+    cout << "Total price: $" << totalPrice << endl;
+
+    return 0;
+}
