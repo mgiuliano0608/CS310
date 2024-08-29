f52d811165c946a52c52d09cbd4e4db71c8d64cc
Michael Giuliano <michael.giuliano.0608@gmail.com>
2024-08-28 21:51:47 -0400

    Added comments

--- Module01/Ch02Exercise13.cpp
+++ Module01/Ch02Exercise13.cpp
@@ -15,6 +15,7 @@ int main()
     float markupPercentage;
     float salesTaxPercentage;
 
+    // Get user input
     cout << "Enter the original price (Ex. 200.00): ";
     cin >> originalPrice;
     cout << "Enter the markup percentage (Ex. 20.0): ";
@@ -22,12 +23,14 @@ int main()
     cout << "Enter the sales tax percentage (Ex. 6.0): ";
     cin >> salesTaxPercentage;
 
+    // Calculate product values
     float markupRate = markupPercentage / 100;
     float salePrice = originalPrice * (1 + markupRate);
     float salesTaxRate = salesTaxPercentage / 100;
     float salesTax = salePrice * salesTaxRate;
     float totalPrice = salePrice + salesTax;
 
+    // Output results
     cout << endl << fixed << setprecision(2);
     cout << "Original price: $" << originalPrice << endl;
     cout << "Markup percentage: " << markupPercentage << "%" << endl;
