d705093df84bee1ce7ae9392f1a2b758aa2898b2
Michael Giuliano <michael.giuliano.0608@gmail.com>
2024-09-05 21:52:00 -0400

    Module 2 discussion with errors

---
+++ Module02/Module02Discussion.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    double sideA, sideB, hypotenuse;
+    double radius, circumference;
+    
+    cout << endl << "--------------------------------------------------" << endl;
+    cout << "What is your first name? ";
+    cin >> name;
+    cout << endl << "Hello " << name << "!" << endl;
+    
+    cout << endl << "Lets calculate the hypotenuse of a right triangle." << endl;
+    cout << "Enter the length of side a: ";
+    cin >> sideA;
+    cout << "Now enter the length of side b: ";
+    cin >> sideB;
+    hypotenuse = sqrt(pow(sideA, 2) + pow(sideB, 2);
+    cout << fixed << setprecision(2);
+    cout << "The hypotenuse of a right triangle with sides " << sideA << " and ";
+         << sideB << " is " << hypotenuse << endl;
+
+    cout << endl << "Great! Now lets calculate the circumference of a circle." << endl;
+    cout << "Enter the circle's radius: ";
+    cin << radius;
+    circumference = 2 x M_PI x radius;
+    cout << "The circumference of a circle with a radius of " << radius << " is " << circumference << endl;
+
+    cout << "Thank you for using this app! Have a great day!" << endl;
+    cout << endl << "--------------------------------------------------" << endl;
+    return 0;
+}
